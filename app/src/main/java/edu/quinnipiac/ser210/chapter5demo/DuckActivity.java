5b170d456828eb518bc146cc268b99663e688395
jjnoga <jjnoga@quinnipiac.edu>
2022-02-08 20:28:36 -0500

    Chapter 5 demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/chapter5demo/DuckActivity.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.chapter5demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DuckActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
